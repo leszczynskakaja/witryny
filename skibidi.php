8a9e6eade310839065689673a20600d15f8c2506
Kaja <79052840+leszczynskakaja@users.noreply.github.com>
2025-09-16 23:31:00 +0200

    Add files via upload

---
+++ skibidi.php
@@ -0,0 +1,15 @@
+<?php
+$konsystencja2 = $_GET['konsystencja'];
+$mleko2 = $_GET['mleko'];
+$tluszcz2 = $_GET['tluszcz'];
+$plastry2 = $_GET['plastry'];
+$opakowania2 = $_GET['opakowania'];
+$ulubieniec2 = $_GET['ulubieniec'];
+
+echo $konsystencja2 . "<br>" . 
+     $mleko2 . "<br>" . 
+     $tluszcz2 . "<br>" . 
+     $plastry2 . "<br>" . 
+     $opakowania2 . "<br>" . 
+     $ulubieniec2;
+?>
